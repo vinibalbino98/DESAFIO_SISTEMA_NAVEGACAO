cb9a0ba443f1d8767d0084e739eebacd755563a6
Vinicius Balbino de Melo <vinibalbino98melo@outlook.com>
2025-08-26 09:14:41 -0400

    Add project files.

---
+++ DESAFIO_SISTEMA_NAVEGACAO/Entities/Browser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Navegacao {
+    public class Browser {
+        private Stack<string> backStack;
+        private Stack<string> forwardStack;
+        private string currentPage;
+
+        public Browser() {
+            backStack = new Stack<string>();
+            forwardStack = new Stack<string>();
+            currentPage = "home"; // página inicial
+        }
+
+        public void Access(string url) {
+            backStack.Push(currentPage);
+            currentPage = url;
+            forwardStack.Clear(); // limpa o histórico futuro
+        }
+
+        public void Back() {
+            if(backStack.Count == 0) {
+                Console.WriteLine("Error: Back error");
+                return;
+            }
+
+            forwardStack.Push(currentPage);
+            currentPage = backStack.Pop();
+        }
+
+        public void Forward() {
+            if(forwardStack.Count == 0) {
+                Console.WriteLine("Error: Forward error");
+                return;
+            }
+
+            backStack.Push(currentPage);
+            currentPage = forwardStack.Pop();
+        }
+
+        public string GetCurrentPage() {
+            return currentPage;
+        }
+    }
+}
---
+++ DESAFIO_SISTEMA_NAVEGACAO/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Navegacao {
+    class Program {
+        static void Main(string[] args) {
+            Browser browser = new Browser();
+
+            // Exemplo: Entrada do PDF (pode ser alterada para testar outros cenários)
+            string[] commands = {
+                "get-current",
+                "access,https://amazon.com",
+                "access,https://cnn.com",
+                "get-current",
+                "back",
+                "get-current",
+                "back",
+                "get-current",
+                "back"
+            };
+
+            TestNavigation(browser, commands);
+        }
+
+        static void TestNavigation(Browser browser, string[] commands) {
+            foreach(var command in commands) {
+                if(command.StartsWith("access")) {
+                    string[] parts = command.Split(',');
+                    browser.Access(parts[1]);
+                }
+                else if(command == "back") {
+                    browser.Back();
+                }
+                else if(command == "forward") {
+                    browser.Forward();
+                }
+                else if(command == "get-current") {
+                    Console.WriteLine(browser.GetCurrentPage());
+                }
+            }
+        }
+    }
+}
